27360c961cce1ad742af6e8ea3aec2d8e4518d02
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-21 09:45:12 +0500

    learning

--- TypeScript/ex-1/app.js
+++ TypeScript/ex-1/app.js
@@ -1,12 +1,27 @@
 // const person: { name: string; age: number; nickname?: string } = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: "dave",
+//     age: 42,
+//     hobbies: ["Sports", "Cooking"],
+//     role: [2, "author"]
+// };
+var ADMIN = 0;
+var READ_ONLY = 1;
+var AUTHOR = 2;
 var person = {
     name: "dave",
     age: 42,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "author"]
+    role: ADMIN
 };
-person.role.push('admin');
+// person.role.push('admin');
 // person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
 var favroiteActivities;
 favroiteActivities = ["Sports"];
 console.log(person.name);
@@ -14,4 +29,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     // console.log(hobby.map());
+}
+if (person.role === ADMIN) {
+    console.log("is admin");
 }
--- TypeScript/ex-1/app.ts
+++ TypeScript/ex-1/app.ts
@@ -1,15 +1,28 @@
 // const person: { name: string; age: number; nickname?: string } = {
 
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: "dave",
+//     age: 42,
+//     hobbies: ["Sports", "Cooking"],
+//     role: [2, "author"]
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 5, READ_ONLY, AUTHOR };
+
+const person = {
     name: "dave",
     age: 42,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "author"]
+    role: Role.ADMIN
 };
 
 // person.role.push('admin');
@@ -25,4 +38,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map());
+}
+
+if (person.role === Role.AUTHOR) {
+    console.log("is author");
 }
