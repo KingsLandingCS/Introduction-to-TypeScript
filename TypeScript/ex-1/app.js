699dd13db7a2115f261fdd9784dd7346163f46e5
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-21 13:10:24 +0500

    learning

--- TypeScript/ex-1/app.js
+++ TypeScript/ex-1/app.js
@@ -1,20 +1,11 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result: ' + num);
-    return;
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-console.log(printResult(add(5, 12)));
-var combineValues;
-var combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+generateError('An error occurred!', 500);
