dc638dc3d5d2c05dd44cce656ad3b37586f64c4b
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-21 09:31:26 +0500

    learning

--- TypeScript/ex-1/app.ts
+++ TypeScript/ex-1/app.ts
@@ -1,8 +1,28 @@
-const person: { name: string; age: number; nickname?: string } = {
-    
-// const person = {
-name: "dave",
+// const person: { name: string; age: number; nickname?: string } = {
+
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+    name: "dave",
     age: 42,
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"]
 };
 
-console.log(person.nickname);
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
+
+let favroiteActivities: string[];
+favroiteActivities = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map());
+}
